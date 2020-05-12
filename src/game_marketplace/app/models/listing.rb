20e09459c7e7fcc207a843a95bd7020259a3619e
Wu94 <wu_jeremy94@outlook.com>
2020-05-12 11:35:45 +1000

    models

---
+++ src/game_marketplace/app/models/listing.rb
@@ -0,0 +1,6 @@
+class Listing < AppicationRecord
+    belong_to :genre
+    belong_to :platform
+    validates :title, :description, :platform_id, :genre_id, :price, :city, :state, :date_of_listing, presence: true
+    has_one_attached :picture
+end
