59bd4ba608316f2707238002cf423a33b5537c6a
Wu94 <wu_jeremy94@outlook.com>
2020-05-12 13:40:40 +1000

    devise

--- src/game_marketplace/app/models/listing.rb
+++ src/game_marketplace/app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < AppicationRecord
     belong_to :platform
     validates :title, :description, :platform_id, :genre_id, :price, :city, :state, :date_of_listing, presence: true
     has_one_attached :picture
+    belongs_to :user
 end
